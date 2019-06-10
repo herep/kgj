c3c37bb58bb9f82c70251dc8c822faf56eea27f0
herex <814322587@qq.com>
2019-06-10 14:37:58 +0800

    入口文件

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ import (
 
 func init() {
 
+	//conf 信息
 	mysql_username := beego.AppConfig.String("kg_mysql_name")
 	mysql_password := beego.AppConfig.String("kg_mysql_password")
 	mysql_host := beego.AppConfig.String("kg_mysql_host")
 	mysql_dbname := beego.AppConfig.String("kg_mysql_dbname")
 
-	//数据库连接 -- kuguanjia
+	//数据库连接 -- kuguanjia--
 	if err := orm.RegisterDataBase("default", "mysql", mysql_username+":"+mysql_password+"@tcp("+mysql_host+":3306)/"+mysql_dbname+"?charset=utf8"); err != nil {
 		fmt.Println(err)
 	}
